208f96fd6fb2e6d4a1444b4cbc8f603967a29c82
stillgood96 <a787574@naver.com>
2023-12-29 10:11:22 +0900

    OutputStream으로 저장처리하지 않고 메모리상에서 encode 해서 response로 내려주기

--- src/main/java/com/eda/image/entertextintoimage/controller/TestController2.java
+++ src/main/java/com/eda/image/entertextintoimage/controller/TestController2.java
@@ -8,10 +8,14 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -26,14 +30,15 @@ public class TestController2 {
   @RequestMapping(value = "/enter-text2", method = { RequestMethod.POST})
   ResponseEntity<Resource> EnterText(@ModelAttribute RequestTestDto dto) throws IOException {
 
-    GifDecoder gifDecoder = new GifDecoder();
+    InputStream inputStream = dto.getPicture().getInputStream();
 
-    MultipartFile picture = dto.getPicture();
-    InputStream inputStream = picture.getInputStream();
+    GifDecoder gifDecoder = new GifDecoder();
     gifDecoder.read(new BufferedInputStream(inputStream));
 
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
     AnimatedGifEncoder encoder = new AnimatedGifEncoder();
-    encoder.start(new FileOutputStream("/Users/imlsw96/Desktop/test.gif"));
+    encoder.start(outputStream);
 
     for(int i = 0; i < gifDecoder.getFrameCount(); i++) {
       BufferedImage frameWithText = addTextToFrame(gifDecoder.getFrame(i), "HelloWorld!!!");
@@ -42,7 +47,16 @@ public class TestController2 {
 
     encoder.finish();
 
-    return null;
+
+    byte[] gifBytes = outputStream.toByteArray();
+
+    // 파일 이름 및 확장자 설정
+    String fileName = dto.getPicture().getOriginalFilename();
+
+    return ResponseEntity.ok()
+        .contentType(MediaType.parseMediaType("image/gif"))
+        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName)
+        .body(new ByteArrayResource(gifBytes));
   }
 
   private BufferedImage addTextToFrame(BufferedImage frame, String text) {
